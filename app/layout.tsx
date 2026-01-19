8193609226bc450abd8d5a5453b90496025607f3
Adao-Angelo <adaobegginer@gmail.com>
2026-01-19 11:22:11 +0100

    refactor:favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,19 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Plus_Jakarta_Sans } from 'next/font/google';
+import './globals.css';
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-plus-jakarta',
+});
 
 export const metadata: Metadata = {
-  title: "MENTORIZA DASHBOARD",
-  description: "Backoff of mentoriza.",
+  title: 'MENTORIZA DASHBOARD',
+  description: 'Back-office da Mentoriza.',
+  icons: {
+    icon: '/favicon.png',
+  },
 };
 
 export default function RootLayout({
@@ -12,8 +22,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+    <html lang='pt' suppressHydrationWarning>
+      <head></head>
+      <body className={` ${plusJakartaSans.variable} antialiased font-body`}>
+        {children}
+      </body>
     </html>
   );
 }
